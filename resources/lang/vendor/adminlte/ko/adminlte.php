6f475d57089453bfee61b0d39dd72ed7773358f5
mogunogu <mogunogu@naver.com>
2020-08-06 12:05:41 +0900

    추가: 관리화면에 어드민lte템플릿 도입

---
+++ resources/lang/vendor/adminlte/ko/adminlte.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    'full_name'                   => '이름',
+    'email'                       => '이메일',
+    'password'                    => '비밀번호',
+    'retype_password'             => '다시입력',
+    'remember_me'                 => '비밀번호 저장',
+    'register'                    => '등록',
+    'register_a_new_membership'   => '회원가입',
+    'i_forgot_my_password'        => '비밀번호를 잃어버렸을때',
+    'i_already_have_a_membership' => '이미 계정을 가지고있습니다',
+    'sign_in'                     => '로그인',
+    'log_out'                     => '로그아웃',
+    'toggle_navigation'           => '네비게이션 시작',
+    'login_message'               => '로그인화면',
+    'register_message'            => '계정을 등록합니다',
+    'password_reset_message'      => '비밀번호 초기화',
+    'reset_password'              => '비밀번호를 초기화합니다',
+    'send_password_reset_link'    => '비밀번호 리셋링크를 보냅니다',
+    'verify_message'              => '계정이 미인증 상태입니다',
+    'verify_email_sent'           => '설정하신 이메일 계정에 인증메일을 발송하였습니다',
+    'verify_check_your_email'     => '회원가입을 완료하시기 위해서는 인증용 URL을 담고있는 이메일을 확인해주시기 바랍니다.',
+    'verify_if_not_recieved'      => '메일이 도착하지 않을경우',
+    'verify_request_another'      => '이곳을 클릭하여 인증메일을 재전송',
+];
---
+++ resources/lang/vendor/adminlte/ko/menu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'main_navigation'               => '메인메뉴',
+    'blog'                          => '블로그',
+    'pages'                         => '페이지',
+    'account_settings'              => '계정설정',
+    'profile'                       => '프로필',
+    'change_password'               => '비밀번호 변경',
+    'multilevel'                    => '멀티메뉴',
+    'level_one'                     => '1단메뉴',
+    'level_two'                     => '2단메뉴',
+    'level_three'                   => '3단메뉴',
+    'labels'                        => '라벨',
+    'important'                     => '중요',
+    'warning'                       => '경고',
+    'information'                   => '정보',
+];
